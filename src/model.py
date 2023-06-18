fdd9173e0e49186b966d600ded6fd882a6e19c7b
MuhammedM294 <muhammedaabdelaal@gmail.com>
2023-06-19 00:58:23 +0200

    add the UNet model

---
+++ src/model.py
@@ -0,0 +1,56 @@
+import segmentation_models_pytorch as smp
+from segmentation_models_pytorch.losses import DiceLoss
+import torch.nn as nn
+
+ENCOUDER = 'resnet34'
+ENCODER_WEIGHTS = 'imagenet'
+
+class UNet(nn.Module):
+    """
+    A PyTorch implementation of the U-Net architecture for semantic segmentation.
+
+    This class provides a wrapper around the U-Net model from the segmentation_models_pytorch library,
+    allowing easy customization of the encoder, encoder weights, number of classes, and activation function.
+
+    Args:
+        encoder_name (str): The name of the pre-trained encoder to use. 
+        encoder_weights (str): The source of the pre-trained encoder weights. 
+        classes (int): The number of classes for the segmentation task. Defaults to 1.
+        activation (str or callable): The activation function to use. Defaults to None.
+
+    Attributes:
+        model (nn.Module): The U-Net model instance from the segmentation_models_pytorch library.
+    """
+    def __init__(self, encoder_name=ENCOUDER, encoder_weights=ENCODER_WEIGHTS, classes=1, activation=None):
+        
+        """
+        Initializes a new instance of UNet.
+        """
+        
+        super(UNet,self).__init__()
+
+        self.model = smp.Unet( encoder_name=encoder_name,
+                               encoder_weights=encoder_weights, 
+                               in_channels=3,
+                               classes=classes,
+                               activation=activation)
+    
+    def forward(self, images, masks = None):
+
+        """
+        Performs forward pass of the U-Net model.
+
+        Args:
+            images (torch.Tensor): Input images to be processed.
+            masks (torch.Tensor, optional): Target masks for calculating the loss. Defaults to None.
+
+        Returns:
+            torch.Tensor: Logits produced by the U-Net model.
+            torch.Tensor: Loss value if masks are provided, otherwise just the logits.
+        """
+
+        logits = self.model(images)
+        if masks != None:
+            loss = DiceLoss(mode='binary')(logits, masks)+ nn.BCEWithLogitsLoss()(logits, masks)
+            return logits, loss
+        return logits
