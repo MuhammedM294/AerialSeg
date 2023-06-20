ae6a711cddc521bb94ccc359b4b4eb08bbc3b111
MuhammedM294 <muhammedaabdelaal@gmail.com>
2023-06-20 11:00:53 +0200

    fix images paths issue

--- src/dataset.py
+++ src/dataset.py
@@ -7,8 +7,7 @@ import os
 import torch
 import albumentations as A
 IMAGE_SIZE = 512
-df_path = 'data/roads/metadata.csv'
-parent_dir = os.path.split(df_path)[0]
+
 
 class SegmentationDataset(Dataset[any]):
     """
@@ -71,13 +70,13 @@ class SegmentationDataset(Dataset[any]):
         return image, mask
     
     def get_image(self, idx:int):
-        image_path = parent_dir+'/'+ self.df.iloc[idx, 4]
+        image_path = self.df.iloc[idx, 2]
         image = cv2.imread(image_path)[:,:,::-1]
         image = cv2.resize(image/255., (IMAGE_SIZE, IMAGE_SIZE))
         return image
     
     def get_mask(self, idx:int):
-        mask_path = parent_dir +'/'+ self.df.iloc[idx, 5]
+        mask_path = self.df.iloc[idx, 3]
         mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
         mask = cv2.resize(mask/255, (IMAGE_SIZE, IMAGE_SIZE))
         mask = np.expand_dims(mask, axis=-1)
